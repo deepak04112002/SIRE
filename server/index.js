a2cc77b53a12dc28ef160b6c1238fe2cf53edd92
Your Name <you@example.com>
2023-09-14 12:09:18 +0530

    password changed

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5005;
 
 dotenv.config();
 
-const url = "mongodb+srv://santoskdas:ewarn@123@cluster0.7z6h5y8.mongodb.net/";
+const url = "mongodb+srv://santoskdas:ewarn123@cluster0.7z6h5y8.mongodb.net/";
 
 const connect = async () => {
   try {
